66445b85bfd57f607f443ac7eff04f2acd4a121d
dominikp <dominik.paprotny@pik-potsdam.de>
2024-10-11 15:22:53 +0200

    Transfer repository

---
+++ exposure/fixed_assets_gapfill.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from copulas.bivariate import Frank
+from scipy.stats import rankdata, spearmanr
+
+def copula_inference_Frank(copula,u,v):
+
+    theta = copula[0]
+    X = copula[1]
+    Y = copula[2]
+
+    V = np.interp(v, X[:,1], X[:,0])
+
+    a = -(1 / theta)
+    b = 1 - np.exp( - theta)
+    c = u ** -1 - 1
+    d = np.exp( -theta * V)
+    f = a * np.log( 1 - b / (c * d +1 ))
+    f[f>1]=1
+    sp = np.interp(f, Y[:,0], Y[:,1])
+
+    return sp
+
+exposure_file = 'C:/HANZE2_products/Compass_exposure/National_exposure_all.xlsx'
+Fixed_asset_raw = pd.read_excel(open(exposure_file, 'rb'), sheet_name='Fixed_assets_to_GDP_raw', index_col='ISOn')
+GDPpc = pd.read_excel(open(exposure_file, 'rb'), sheet_name='GDP_per_capita_2017$', index_col='ISOn')
+Pop = pd.read_excel(open(exposure_file, 'rb'), sheet_name='Population', index_col='ISOn')
+Countries = range(0,248)
+Years = range(1850,2024)
+
+Select_years = [3,13,23,33,43,53,66,73,82,91,103,113,123,133,143,153,163,172]
+Fixed_asset_raw_a = Fixed_asset_raw.values[:,Select_years].astype(float)
+Fixed_asset_raw_av = Fixed_asset_raw_a.reshape(-1,1)
+
+GDPpc_a = GDPpc.values[:,Select_years].astype(float)
+GDPpc_av = GDPpc_a.reshape(-1,1)
+
+Combined_data = np.concatenate([GDPpc_av,Fixed_asset_raw_av],axis=1)
+Combined_data_d = Combined_data[~np.isnan(Combined_data).any(axis=1)]
+ix = (Combined_data_d>0) & (Combined_data_d<10)
+Combined_data_dz = Combined_data_d[ix.any(axis=1)]
+
+#np.savetxt('C:/HANZE2_products/Compass_exposure/Fixed_asset_GDPpc.csv', Combined_data_dz, delimiter=",", fmt="%f")
+
+### Fit Gumbel copula to the data
+Combined_data_ranked = rankdata(Combined_data_dz, method='min', axis=0) / (Combined_data_dz.shape[0] + 1)
+copula = Frank()
+r = spearmanr(Combined_data_ranked)
+copula.fit(Combined_data_ranked)
+param = copula.theta
+X_margins = np.sort(np.stack([Combined_data_ranked[:, 0], Combined_data_dz[:, 0]], axis=1), axis=0)
+Y_margins = np.sort(np.stack([Combined_data_ranked[:, 1], Combined_data_dz[:, 1]], axis=1), axis=0)
+copula_assets = [param, X_margins, Y_margins]
+rng = np.random.default_rng(12345)
+copula_samples = rng.uniform(low=0, high=1, size=10000)
+
+### Estimate fixed asset per country and year
+GDPpc_all = GDPpc.values[:,3:].astype(float)
+Fixed_asset_raw_all = Fixed_asset_raw.values[:,3:].astype(float)
+Fixed_asset_pred = np.zeros([len(Countries),len(Years)])
+for c in Countries:
+    print(str(c))
+    Fixed_asset_pred_c = np.zeros([1,len(Years)])
+    for y in Years:
+        GDPpc_cy = GDPpc_all[c,y-1850]
+        Fixed_asset_pred_c[0,y-1850] = np.mean(copula_inference_Frank(copula_assets, copula_samples, GDPpc_cy))
+    asset_avail = ~np.isnan(Fixed_asset_raw_all[c, :])
+    if sum(asset_avail)==0:
+        Fixed_asset_pred[c,:] = Fixed_asset_pred_c
+    else:
+        Fixed_asset_pred[c, asset_avail] = Fixed_asset_raw_all[c, asset_avail]
+        Index_first = np.where(asset_avail == 1)[0][0]
+        Index_last = np.where(asset_avail == 1)[0][-1]
+        Fixed_asset_first = Fixed_asset_raw_all[c, Index_first]
+        Fixed_asset_last = Fixed_asset_raw_all[c, Index_last]
+        Fixed_asset_pred[c, 0:Index_first] = Fixed_asset_pred_c[0,0:Index_first] * Fixed_asset_first / Fixed_asset_pred_c[0,Index_first]
+        Fixed_asset_pred[c, Index_last:] = Fixed_asset_pred_c[0,Index_last:] * Fixed_asset_last / Fixed_asset_pred_c[0,Index_last]
+
+np.savetxt('C:/HANZE2_products/Compass_exposure/Fixed_asset_prediction.csv', Fixed_asset_pred, delimiter=",", fmt="%f")
---
+++ input_meteo_data/cds_Xynthia_data_download.py
@@ -0,0 +1,73 @@
+import cdsapi
+import os
+
+c = cdsapi.Client()
+
+years = range(1950,2024)
+months = range(1,13)
+
+for y in years:
+    for m in months:
+
+        ## ERA5-Land
+        if os.path.isfile('/p/tmp/dominikp/COMPASS/Meteo_data/Wind_ERA5Land/ERA5Land_sfcWind_'+str(y)+'_'+str(m)+'.grib'):
+            print('Already downloaded')
+        else:
+            c.retrieve(
+                'reanalysis-era5-land',
+                {
+                    'variable': [
+                        '10m_u_component_of_wind', '10m_v_component_of_wind',
+                    ],
+                    'year': str(y),
+                    'month': str(m),
+                    'day': [
+                        '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                        '10', '11', '12', '13', '14', '15', '16', '17', '18',
+                        '19', '20', '21', '22', '23', '24', '25', '26', '27',
+                        '28', '29', '30', '31',
+                    ],
+                    'time': [
+                        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+                        '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
+                        '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+                        '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+                    ],
+                    'area': [
+                        52, -6, 41, 10, ## France
+                    ],
+                    'format': 'grib',
+                },
+                '/p/tmp/dominikp/COMPASS/Meteo_data/Wind_ERA5Land/ERA5Land_sfcWind_'+str(y)+'_'+str(m)+'.grib')
+
+        ## additional ERA5 data for gap-filling in coastal areas
+        if os.path.isfile('/p/tmp/dominikp/COMPASS/Meteo_data/Wind_ERA5/ERA5_sfcWind_'+str(y)+'_'+str(m)+'.grib'):
+            print('Already downloaded')
+        else:
+            c.retrieve(
+                'reanalysis-era5-single-levels',
+                {
+                    'product_type': 'reanalysis',
+                    'variable': [
+                        '10m_u_component_of_wind', '10m_v_component_of_wind',
+                    ],
+                    'year': str(y),
+                    'month': str(m),
+                    'day': [
+                        '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                        '10', '11', '12', '13', '14', '15', '16', '17', '18',
+                        '19', '20', '21', '22', '23', '24', '25', '26', '27',
+                        '28', '29', '30', '31',
+                    ],
+                    'time': [
+                        '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+                        '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
+                        '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+                        '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+                    ],
+                    'area': [
+                        52, -6, 41, 10, ## France
+                    ],
+                    'format': 'grib',
+                },
+                '/p/tmp/dominikp/COMPASS/Meteo_data/Wind_ERA5/ERA5_sfcWind_'+str(y)+'_'+str(m)+'.grib')
